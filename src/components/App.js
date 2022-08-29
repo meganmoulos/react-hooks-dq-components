7cb586cb1e4ee6c7076bc515014fe5f5c2bea388
Megan <meganmoulos@gmail.com>
2022-08-29 15:10:48 -0700

    Done with DQ

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import SideMenu from './SideMenu.js'
+import MainContent from './MainContent.js'
+import TopMenu from './TopMenu'
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+  <div className="app-container">
+    <SideMenu />
+    <TopMenu />
+    <MainContent />
+  </div>
+  )
 }
 
 export default App;
